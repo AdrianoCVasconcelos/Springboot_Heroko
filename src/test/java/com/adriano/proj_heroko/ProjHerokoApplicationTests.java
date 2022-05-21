3146589dcd1b3cf01b94e5eab4137a1adc1dbb24
AdriAnoCVasconcelos <adriano.cvasconcelos@hotmail.com>
2022-05-21 15:29:11 -0300

    Project created

---
+++ src/test/java/com/adriano/proj_heroko/ProjHerokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriano.proj_heroko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjHerokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
